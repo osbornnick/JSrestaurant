ea1b4880ad282c14a2492b4a79f5793f439f866d
Nicholas Osborn <nicholas.p.osborn@gmail.com>
2020-06-05 10:49:27 -0400

    added about and menu

--- src/index.js
+++ src/index.js
@@ -2,5 +2,52 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
+import {render as renderHome} from './home';
+import {render as renderMenu} from './menu';
+import {render as renderAbout} from './about';
 
-console.log('hello');
+function reset() {
+  const content = document.querySelector('.content');
+  content.className = 'content row';
+  content.innerHTML = '';
+}
+
+function setup() {
+  reset();
+  renderHome();
+  document.querySelectorAll('.nav-item').forEach(nav => {
+    nav.addEventListener('click', e => {
+      reset();
+      const id = e.target.id;
+      activateMenu(id);
+      renderChoice(id);
+    });
+  });
+}
+
+function activateMenu(menu) {
+  Array.from(document.querySelector('.navbar-nav').children).forEach(child => {
+    child.classList.remove('active');
+  });
+
+  document.querySelector(`#${menu}`).classList.add('active');
+}
+
+function renderChoice(page) {
+  switch (page) {
+    case 'home':
+      renderHome();
+      break;
+    case 'menu':
+      renderMenu();
+      break;
+    case 'about':
+      renderAbout();
+      break;
+    // case 'contact':
+    //   renderContact();
+    //   break;
+  }
+}
+
+setup();
